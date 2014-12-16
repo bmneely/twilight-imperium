54884a8d353bfda526485a491d041fd0b146b298
Ben Neely <bmneely@gmail.com>
2014-12-15 20:48:35 -0700

    Authorization

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -12,12 +12,15 @@ class PlayersController < ApplicationController
     @users = User.all.reject{|x| @game.users.include? x.id}
 
     @player = Player.new
+    authorize @player
   end
 
   def create
     @game = Game.find(params[:game_id])
     player = @game.players.create(params.require(:player).permit(:user_id))
+    authorize player
 
     redirect_to @game, :flash => { :notice => "Player added!" }
+
   end
 end
