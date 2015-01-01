6146d5b2f4194b0a7dd2de8c3442255799bdd9ad
Ben Neely <bmneely@gmail.com>
2015-01-01 16:57:32 -0700

    Reveal objectives with save

--- app/controllers/objective_cards_controller.rb
+++ app/controllers/objective_cards_controller.rb
@@ -3,10 +3,9 @@ class ObjectiveCardsController < ApplicationController
     @game = Game.find(params[:game_id])
     objective_card = ObjectiveCard.find(params[:objective_card_id])
     objective_card.reveal_card
-    
+
     flash[:notice] = "Public Objective was revealed"
     redirect_to @game
-
   end
 
 end
--- app/models/objective_card.rb
+++ app/models/objective_card.rb
@@ -2,8 +2,8 @@ class ObjectiveCard < ActiveRecord::Base
   belongs_to :public_objective_deck
 
   def reveal_card
-    revealed = true
-    save!
+    self.revealed = true
+    self.save!
 
     CardMailer.new_public_objective_card(public_objective_deck.game, self).deliver
   end
